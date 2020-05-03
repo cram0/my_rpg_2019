d5f9b308a539a11f412f836cd0154d7f7918a1b1
Tanguy Andreani <tanguy.andreani@tuta.io>
2020-05-03 18:08:27 +0200

    fixed: destroying scene at its end

--- core/game_loop/keyboard_events.c
+++ core/game_loop/keyboard_events.c
@@ -8,7 +8,12 @@
 void press_q_to_quit(global_state *state)
 {
     if (state->event.type == sfEvtKeyPressed) {
-        if (state->event.key.code == sfKeyQ)
+        if (state->event.key.code == sfKeyQ) {
+            int old_id = state->scene_id;
+            if (old_id > -1)
+                state->scenes_db[old_id].destroy(state->scene_state);
+
             sfRenderWindow_close(state->win);
+        }
     }
 }
